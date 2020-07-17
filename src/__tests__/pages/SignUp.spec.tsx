b4e608df66f141dd40d5fd804ffd9bd36050d5f8
danielmorita <danielkeyti@gmail.com>
2020-07-17 09:05:12 -0300

    gobarber-web

---
+++ src/__tests__/pages/SignUp.spec.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import { render, fireEvent, wait } from '@testing-library/react';
+import MockAdapter from 'axios-mock-adapter';
+import api from '../../services/api';
+
+import SignUp from '../../pages/SignUp';
+
+const apiMock = new MockAdapter(api);
+
+const mockedHistoryPush = jest.fn();
+const mockedAddToast = jest.fn();
+
+jest.mock('react-router-dom', () => {
+  return {
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
+    Link: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
+
+jest.mock('../../hooks/toast', () => {
+  return {
+    useToast: () => ({
+      addToast: mockedAddToast,
+    }),
+  };
+});
+
+describe('SignUp Page', () => {
+  beforeEach(() => {
+    mockedHistoryPush.mockClear();
+  });
+
+  it('should be able to sign up', async () => {
+    const apiResponse = {
+      user: {
+        id: 'user-123',
+        name: 'John Doe',
+        email: 'johndoe@example.com.br',
+      },
+      token: 'token-123',
+    };
+    apiMock.onPost('users').reply(200, apiResponse);
+
+    const { getByPlaceholderText, getByText } = render(<SignUp />);
+
+    const nameField = getByPlaceholderText('Nome');
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+    const buttonElement = getByText('Cadastrar');
+
+    fireEvent.change(nameField, { target: { value: 'johndoe' } });
+    fireEvent.change(emailField, { target: { value: 'johndoe@example.com' } });
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+
+    fireEvent.click(buttonElement);
+
+    await wait(() => {
+      expect(mockedHistoryPush).toHaveBeenCalledWith('/');
+    });
+  });
+
+  it('should not be able to sign up with invalid e-mail', async () => {
+    const { getByPlaceholderText, getByText } = render(<SignUp />);
+
+    const nameField = getByPlaceholderText('Nome');
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+    const buttonElement = getByText('Cadastrar');
+
+    fireEvent.change(nameField, { target: { value: 'johndoe' } });
+    fireEvent.change(emailField, { target: { value: 'not-valid-email' } });
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+
+    fireEvent.click(buttonElement);
+
+    await wait(() => {
+      expect(mockedHistoryPush).not.toHaveBeenCalled();
+    });
+  });
+
+  it('should display an error if logout fails', async () => {
+    // mockedSignUp.mockImplementation(() => {
+    //   throw new Error();
+    // });
+    apiMock.onPost('users').reply(500, {});
+    const { getByPlaceholderText, getByText } = render(<SignUp />);
+
+    const nameField = getByPlaceholderText('Nome');
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+    const buttonElement = getByText('Cadastrar');
+
+    fireEvent.change(nameField, { target: { value: 'johndoe' } });
+    fireEvent.change(emailField, { target: { value: 'johndoe@example.com' } });
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+
+    fireEvent.click(buttonElement);
+
+    await wait(() => {
+      expect(mockedAddToast).toHaveBeenCalledWith(
+        expect.objectContaining({ type: 'error' }),
+      );
+    });
+  });
+});
